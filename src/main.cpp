5309d10a2096189279b8f0539b632cd08fd86b00
George Wilkins <jaffawilkins@gmail.com>
2024-08-18 20:05:28 +1200

    added version 3.8 kernel

--- src/main.cpp
+++ src/main.cpp
@@ -73,22 +73,19 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+#define LEFT_WHEELS_PORT 1
+#define RIGHT_WHEELS_PORT 10
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+  pros::Motor left_wheels (LEFT_WHEELS_PORT);
+  pros::Motor right_wheels (RIGHT_WHEELS_PORT); // This reverses the motor
+  pros::Controller master (CONTROLLER_MASTER);
 
+  while (true) {
+    left_wheels.move(master.get_analog(ANALOG_LEFT_Y));
+    right_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+    pros::delay(2);
+  }
 }
